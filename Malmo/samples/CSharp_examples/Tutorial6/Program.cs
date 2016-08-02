2799a5c9124780a33d45132a0fb1097964094323
Marvin Varela <marvin.varela@outlook.com>
2016-08-01 19:40:55 -0600

    * Adding tutorial 6 (solved, got to remove the solution)

---
+++ Malmo/samples/CSharp_examples/Tutorial6/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace Tutorial6
+{
+    using Microsoft.Research.Malmo;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            //sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+            Console.Clear();
+
+            //agent = TabQAgent()
+            var agent = new TabQAgent();
+            //agent_host = MalmoPython.AgentHost()
+            AgentHost agentHost = new AgentHost();
+            //try:
+            //    agent_host.parse(sys.argv)
+            //except RuntimeError as e:
+            //    print 'ERROR:',e
+            //    print agent_host.getUsage()
+            //    exit(1)
+            //if agent_host.receivedArgument("help"):
+            //    print agent_host.getUsage()
+            //    exit(0)
+            try
+            {
+                agentHost.parse(new StringVector(Environment.GetCommandLineArgs()));
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("ERROR: {0}", ex.Message);
+                Console.Error.WriteLine(agentHost.getUsage());
+                Environment.Exit(1);
+            }
+            if (agentHost.receivedArgument("help"))
+            {
+                Console.Error.WriteLine(agentHost.getUsage());
+                Environment.Exit(0);
+            }
+
+
+
+            //# -- set up the mission -- #
+            //mission_file = './tutorial_6.xml'
+            //with open(mission_file, 'r') as f:
+            //    print "Loading mission from %s" % mission_file
+            //    mission_xml = f.read()
+            //    my_mission = MalmoPython.MissionSpec(mission_xml, True)
+            //# add 20% holes for interest
+            //for x in range(1, 4):
+            //    for z in range(1, 13):
+            //        if random.random() < 0.1:
+            //            my_mission.drawBlock(x, 45, z, "lava")
+            var currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var missionFilename = Path.Combine(currentPath, "mission.xml");
+            var missionString = System.IO.File.ReadAllText(missionFilename);
+            MissionSpec mission = new MissionSpec(missionString, validate: true);
+            for (int x = 1; x <= 4; x++)
+            {
+                for (int z = 1; z <= 13; z++)
+                {
+                    if (random.NextDouble() <= 0.1)
+                    {
+                        mission.drawBlock(x, 45, z, "lava");
+                    }
+                }
+            }
+            var maxRetries = 3;
+            var numRepeats = 1500;
+            if (agentHost.receivedArgument("test"))
+            {
+                numRepeats = 1;
+            }
+
+            var cumulativeRewards = new List<double>();
+            for (int i = 0; i < numRepeats; i++)
+            {
+                Console.WriteLine($"Repeat {i} of {numRepeats}");
+                MissionRecordSpec missionRecord = new MissionRecordSpec();
+                for (int retryIndex = 0; retryIndex < maxRetries; retryIndex++)
+                {
+                    try
+                    {
+                        agentHost.startMission(mission, missionRecord);
+                        WorldState worldState;
+
+                        Console.WriteLine("Waiting for the mission to start");
+                        do
+                        {
+                            Console.Write(".");
+                            Thread.Sleep(100);
+                            worldState = agentHost.getWorldState();
+
+                            foreach (TimestampedString error in worldState.errors) Console.Error.WriteLine("Error: {0}", error.text);
+                        }
+                        while (!worldState.is_mission_running);
+
+                        Console.WriteLine("Mission running...");
+                        if (worldState.is_mission_running)
+                        {
+                            break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        if (retryIndex == maxRetries - 1)
+                        {
+                            Console.Error.WriteLine("Error starting mission: {0}", ex.Message);
+                            Environment.Exit(1);
+                        }
+                        else
+                        {
+                            Thread.Sleep(2500);
+                        }
+                    }
+                }
+                var cumulativeReward = agent.Run(agentHost);
+                Console.WriteLine($"Cumulative reward: {cumulativeReward}");
+                cumulativeRewards.Add(cumulativeReward);
+                Thread.Sleep(500); // (let the Mod reset)
+            }
+            Console.WriteLine("Done.");
+            Console.WriteLine();
+            Console.WriteLine($"Cumulative rewards for all {numRepeats} runs: {string.Join(",", cumulativeRewards)}");
+        }
+    }
+}
---
+++ Malmo/samples/CSharp_examples/Tutorial6/TabQAgent.cs
@@ -0,0 +1,264 @@
+﻿namespace Tutorial6
+{
+    using Microsoft.Research.Malmo;
+    using Newtonsoft.Json.Linq;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    struct RouteStep
+    {
+        public string State { get; private set; }
+        public string Action { get; private set; }
+        public RouteStep(string state, string action)
+        {
+            State = state;
+            Action = action;
+        }
+        public override int GetHashCode()
+        {
+            return $"{State}{Action}".GetHashCode();
+        }
+        public override bool Equals(object ob)
+        {
+            if (ob is RouteStep)
+            {
+                RouteStep r = (RouteStep)ob;
+                return State == r.State && Action == r.Action;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator ==(RouteStep r1, RouteStep r2)
+        {
+            return r1.Equals(r2);
+        }
+        public static bool operator !=(RouteStep r1, RouteStep r2)
+        {
+            return !r1.Equals(r2);
+        }
+    }
+
+    class TabQAgent
+    {
+        public double Epsilon { get; set; } = 0.1;
+        public string[] Actions { get; set; } = new string[] { "movenorth 1", "movesouth 1", "movewest 1", "moveeast 1" };
+        public Dictionary<string, Dictionary<string, double>> QTable { get; set; } = new Dictionary<string, Dictionary<string, double>>();
+        public string PreviousState { get; set; }
+        public string PreviousAction { get; set; }
+        List<RouteStep> route = new List<RouteStep>();
+        Random random = new Random();
+        double maxReward = double.MinValue;
+        public void UpdateQTable(double reward)
+        {
+            //Change q_table to reflect what we have learnt.
+
+            //retrieve the old action value from the Q-table (indexed by the previous state and the previous action)
+            var oldQ = QTable[PreviousState][PreviousAction];
+
+            //# TODO: what should the new action value be?
+            var newQ = (oldQ + reward) / 2;
+
+            //# assign the new action value to the Q-table
+            QTable[PreviousState][PreviousAction] = newQ;
+        }
+        public void UpdateQTableFromTerminatingState(double reward)
+        {
+
+            ////Change q_table to reflect what we have learnt.
+
+            ////retrieve the old action value from the Q-table (indexed by the previous state and the previous action)
+            //var oldQ = QTable[PreviousState][PreviousAction];
+
+            ////# TODO: what should the new action value be?
+            //var newQ = oldQ;
+
+            ////# assign the new action value to the Q-table
+            //QTable[PreviousState][PreviousAction] = newQ;
+            //************************************
+
+
+            //Change q_table to reflect what we have learnt, after reaching a terminal state.
+
+            maxReward = Math.Max(maxReward, reward);
+            var multiplicator = 2;
+            if (reward == maxReward)
+            {
+                Epsilon -= 0.001;
+                multiplicator = 10;
+                Console.WriteLine("*************************************");
+                Console.WriteLine($"****NEW MAX: {maxReward}************");
+                Console.WriteLine($"****NEW EPSILON: {Epsilon}**********");
+                Console.WriteLine("*************************************");
+            }
+
+            reward *= multiplicator;
+            reward /= route.Count;
+            foreach (var routeStep in route)
+            {
+                //# retrieve the old action value from the Q-table (indexed by the previous state and the previous action)
+                var oldQ = QTable[routeStep.State].ContainsKey(routeStep.Action) ? QTable[routeStep.State][routeStep.Action] : reward;
+
+                //# TODO: what should the new action value be?
+                var newQ = (oldQ + reward) / 2;
+
+                //# assign the new action value to the Q-table
+                QTable[routeStep.State][routeStep.Action] = newQ;
+            }
+            route.Clear();
+        }
+        public void Act(WorldState worldState, AgentHost agentHost)
+        {
+            //"""take 1 action in response to the current world state"""
+            var observationText = worldState.observations[0].text;
+            dynamic observations = JObject.Parse(observationText);
+            //Console.WriteLine(observations);
+            JToken XPos, ZPos;
+            observations.TryGetValue("XPos", out XPos);
+            observations.TryGetValue("ZPos", out ZPos);
+            if (XPos == null || ZPos == null)
+            {
+                Console.WriteLine($"Incomplete observation received: {observationText}");
+                return;
+            }
+            var currentState = $"{observations.XPos}:{observations.ZPos}";
+            Console.WriteLine($"State: {currentState} (x = {XPos}, z = {ZPos})");
+            if (!QTable.ContainsKey(currentState))
+            {
+                QTable[currentState] = new Dictionary<string, double>();
+                foreach (var action in Actions)
+                {
+                    QTable[currentState][action] = 0.0;
+                }
+            }
+            //self.drawQ(curr_x = int(obs[u'XPos']), curr_y = int(obs[u'ZPos']))
+            //# select the next action
+            var actionIndex = 0;
+            if (random.NextDouble() < Epsilon || QTable[currentState].Values.Count == 0)
+            {
+                actionIndex = random.Next(0, Actions.Length);
+                Console.WriteLine($"Random action: {Actions[actionIndex]}");
+            }
+            else
+            {
+                var maxReward = QTable[currentState].Values.Count == 0 ? double.MinValue : QTable[currentState].Values.Max();
+                Console.WriteLine($"Current values: {string.Join(",", QTable[currentState].Values)}");
+                var l = new List<int>();
+                for (int i = 0; i < Actions.Length; i++)
+                {
+                    if (QTable[currentState].ContainsKey(Actions[i]) && QTable[currentState][Actions[i]] == maxReward)
+                    {
+                        l.Add(i);
+                    }
+                }
+                var y = random.Next(0, l.Count);
+                actionIndex = l[y];
+                Console.WriteLine($"Taking Q Action: {Actions[actionIndex]}");
+            }
+
+            try
+            {
+                //# try to send the selected action, only update prev_s if this succeeds
+                agentHost.sendCommand(Actions[actionIndex]);
+                Thread.Sleep(100);
+                PreviousState = currentState;
+                PreviousAction = Actions[actionIndex];
+                var currentStep = new RouteStep(PreviousState, PreviousAction);
+                if (!route.Contains(currentStep)) route.Add(currentStep);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed ot send command: {e}");
+            }
+        }
+        public double Run(AgentHost agentHost)
+        {
+            var totalReward = 0.0;
+            PreviousState = null;
+            PreviousAction = null;
+            var isFirstAction = true;
+
+            var worldState = agentHost.getWorldState();
+            do
+            {
+                var currentReward = 0.0;
+                if (isFirstAction)
+                {
+                    while (true)
+                    {
+                        Thread.Sleep(100);
+                        worldState = agentHost.getWorldState();
+                        foreach (var error in worldState.errors)
+                        {
+                            Console.WriteLine($"Error: {error}");
+                        }
+                        foreach (var reward in worldState.rewards)
+                        {
+                            currentReward += reward.getValue();
+                        }
+                        if (worldState.is_mission_running && worldState.observations.Count > 0 && !(worldState.observations[0].text == "{}"))
+                        {
+                            Act(worldState, agentHost);
+                            totalReward += currentReward;
+                            break;
+                        }
+                        if (!worldState.is_mission_running) break;
+                    }
+                    isFirstAction = false;
+                }
+                else
+                {
+                    //        # wait for non-zero reward
+                    while (worldState.is_mission_running && currentReward == 0.0)
+                    {
+                        Thread.Sleep(200);
+                        worldState = agentHost.getWorldState();
+                        foreach (var error in worldState.errors)
+                        {
+                            Console.WriteLine($"Error: {error}");
+                        }
+                        foreach (var reward in worldState.rewards)
+                        {
+                            currentReward += reward.getValue();
+                        }
+                    }
+                    while (true)
+                    {
+                        Thread.Sleep(200);
+                        worldState = agentHost.getWorldState();
+                        foreach (var error in worldState.errors)
+                        {
+                            Console.WriteLine($"Error: {error}");
+                        }
+                        foreach (var reward in worldState.rewards)
+                        {
+                            currentReward += reward.getValue();
+                        }
+                        if (worldState.is_mission_running && worldState.observations.Count > 0 && !(worldState.observations[0].text == "{}"))
+                        {
+                            Act(worldState, agentHost);
+                            totalReward += currentReward;
+                            break;
+                        }
+                        if (!worldState.is_mission_running) break;
+                    }
+                }
+                //# process final reward
+                Console.WriteLine($"Curent reward: {currentReward}");
+                totalReward += currentReward;
+                if (PreviousState != null && PreviousAction != null)
+                {
+                    UpdateQTable(totalReward);
+                }
+            } while (worldState.is_mission_running);
+            UpdateQTableFromTerminatingState(totalReward);
+            //self.drawQ()
+            return totalReward;
+        }
+    }
+}
